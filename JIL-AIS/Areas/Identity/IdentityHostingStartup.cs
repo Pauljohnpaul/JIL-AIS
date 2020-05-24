8a6287df7945e91daf7638cdf6babad26edb6505
PauL\JpauL <johnpaulgreganda@gmail.com>
2020-05-24 20:30:35 +0800

    Add project files.

---
+++ JIL-AIS/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using JIL_AIS.Areas.Identity.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(JIL_AIS.Areas.Identity.IdentityHostingStartup))]
+namespace JIL_AIS.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<JILDBContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("JILDBContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<JILDBContext>();
+            });
+        }
+    }
+}
---
+++ JIL-AIS/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace JIL_AIS.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
