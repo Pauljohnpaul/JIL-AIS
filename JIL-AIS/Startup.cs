e85a0f9ab912e6c3f62263e0a676ca9bf54edb81
PauL\JpauL <johnpaulgreganda@gmail.com>
2020-05-30 12:26:57 +0800

    Apply template to site.

--- JIL-AIS/Startup.cs
+++ JIL-AIS/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,6 +26,16 @@ namespace JIL_AIS
         {
             services.AddControllersWithViews();
             services.AddRazorPages();
+
+            // configure Identity
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 5;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
